867980cacc2ca73ccb9466909108ce720a1b3152
DeltaStruct <DeltaStruct47@gmail.com>
2025-07-21 11:16:25 +0000

    verify

--- verify/yosupo_static_range_sum.cpp
+++ verify/yosupo_static_range_sum.cpp
@@ -1,4 +1,4 @@
-// PROBLEM: https://judge.yosupo.jp/problem/static_range_sum
+// PROBLEM https://judge.yosupo.jp/problem/static_range_sum
 
 #include "../dp/cumulative_sum.hpp"
 #include <bits/stdc++.h>
